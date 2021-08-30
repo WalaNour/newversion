5fa2c4c53f50a898bd30e116d43724a8811f75a1
WalaNour <nourwala548@gmail.com>
2021-08-30 19:14:14 +0100

    admin dashboard

--- src/app/admin-home/admin-home.component.ts
+++ src/app/admin-home/admin-home.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-admin-home',
   templateUrl: './admin-home.component.html',
-  styleUrls: ['./admin-home.component.css']
+  styleUrls: ['./admin-home.component.css'],
 })
 export class AdminHomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  // x: any = false;
+  // y: any = true;
+  ngOnInit(): void {}
+  // toggle() {
+  //   let menuToggle = document.querySelector('.toggle');
+  //   let navigation = document.querySelector('.navigation');
+  //   this.x = !this.x;
+  //   this.y = !this.y;
+  //   menuToggle.classList.toggle('active');
+  //   navigation.classList.toggle('active');
+  // }
 }
